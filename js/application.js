14b40b7e7cf96064139782006fa48589e284aec8
jpr <jpr@organisiert.net>
2012-07-02 00:47:34 +0200

    * Add font from fontello for nicer icons * Make necessary implementation for handling the basic buttons

--- js/application.js
+++ js/application.js
@@ -5,37 +5,85 @@ $(document).ready(function() {
    $("#start").click(function() {
      switch(state){
       case "ready":
-        startdate = new Date();
+        startdate = new XDate();
         state = "running";
-        timeline_container.append('<h2>Tracking session started at ' +formatDate(startdate)+ '</h2>');
-        timeline_container.append('<ul id="timeline_list">');
-        $("#timeline_list").append('<li>'+formatDate(startdate)+'</li>');
-        timeline_container.append('</ul>');
+        timeline_container.append('<h2>Tracking session started at ' + startdate.toString('dd.MM.yyyy HH:mm:ss') + '</h2>');
+        timeline_container.append(addRow(formatDateDiff(startdate, startdate), 'Intro', startdate.getTime()));
         $("#start").html("Add new Timestamp...");
+        $("#stop").removeClass('hide');
         break;
       case "running":
-        var now = new Date();
-        $("#timeline_list").append('<li>'+now.toString('dd.MM.yyyy HH:mm:ss')+'</li>');
+        var now = new XDate();
+        timeline_container.append(addRow(formatDateDiff(startdate, now), '', now.getTime()));
         break;
       case "finished":
         break;
      }
    });
+   
+   $("#stop").click(function() {
+    var now = new XDate();
+    timeline_container.append(addRow(formatDateDiff(startdate, now), 'End', now.getTime()));
+    state = "finished";
+    startdate = null;
+    timeline_container.append('<h2>Tracking session stopped at ' + now.toString('dd.MM.yyyy HH:mm:ss') + '</h2>');
+    $("#control-buttons").addClass('hide');
+    $("#dl-buttons").removeClass('hide');
+    $("#help").addClass('help-show');
+   });
+
+   $("#reload").click(function() {
+    location.reload();
+   });
+
+   $("#text").click(function() {
+    alert('Download as text');
+   });
+
+   $("#xml").click(function() {
+    alert('Download as XML');
+   });
+
+   $(document).on("click", "a.delete", function(){
+    $(this).closest('div.row').remove(); 
+    return false;
+   });
+
+
  });
 
-// Takes a full date and makes a formatted String 
-// in the form of dd.MM.yyyy HH:mm:ss of it
-function formatDate(date) {
-  var day = date.getDate();
-  if (day < 9) {day = "0" + day};
-  var month = date.getMonth() + 1;
-  if (month < 9) {month = "0" + month};
-  var year = date.getFullYear();
-  var hours = date.getHours();
-  if (hours < 9) {hours = "0" + hours};
-  var minutes = date.getMinutes();
-  if (minutes < 9) {minutes = "0" + minutes};
-  var seconds = date.getSeconds();
-  if (seconds < 9) {seconds = "0" + seconds};
-  return day + "." + month + "." + year + " " + hours + ":" + minutes + ":" + seconds;
+// Diffs all Timefields of two XDates and returns a Timestring containing the result
+function formatDateDiff(startdate, now) {
+  var hours = Math.floor(startdate.diffHours(now));
+  var minutes = Math.floor(startdate.diffMinutes(now));
+  minutes = minutes - hours * 60; // subtract all elapsed hours
+  var seconds = Math.floor(startdate.diffSeconds(now));
+  seconds = seconds - ((hours * 3600) + (minutes * 60)); // subtract all elapsed hours and minutes
+  var milliseconds = startdate.diffMilliseconds(now);
+  milliseconds = milliseconds - ((hours * 3600000) + (minutes * 60000) + (seconds * 1000)); // subtract all elapsed hours, minutes and seconds
+
+  if (hours < 10) { hours = "0" + hours; }
+  if (minutes < 10) { minutes = "0" + minutes; }
+  if (seconds < 10) { seconds = "0" + seconds; }
+  if (milliseconds < 100) { milliseconds = "0" + milliseconds; }
+  
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
+
+function addRow(diff, text, id) {
+
+  var template = '<div class="row">' +
+    '<div class="span4">' +
+      '<input type="text" class="span4" placeholder="Timecode of the starttime" value="'+ diff +'" />' +
+    '</div>' + 
+    '<div class="span5">' +
+      '<input type="text" class="span5" placeholder="Please enter a description..." value="' + text + '" />' +
+    '</div>' + 
+    '<div class="span1">' + 
+      '<a href="#" title="delete this entry" class="icon-cancel delete" id="'+ id +'" />' + 
+    '</div>' + 
+  '</div>';
+
+  return template;
+}
+
